8f8bedf195ef7f4e315c404de3176bb4e0e900cc
MakarukArsen <makarukarsen@gmail.com>
2022-06-18 19:43:50 +0300

    0.5

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("123")
+const buttons = document.querySelector(".buttons");
+console.log(buttons)
+
+function playSound(target){
+    const mp3 = new Audio(`/sounds/${target}.mp3`)
+    mp3.play()
+}
+buttons.addEventListener("click", (e) => {
+    const target = e.target.closest(".button").id
+    if(e.target.closest(".button")){
+        playSound(target)
+    }
+}) 
